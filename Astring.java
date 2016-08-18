c65456562de24506ad200615feab6eb4770810d6
Mayank2204 <mayankgurjar22@gmail.com>
2016-08-18 22:17:06 +0530

    some methods of string class

---
+++ Astring.java
@@ -0,0 +1,29 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package practice;
+import java.lang.String;
+
+
+/**
+ *
+ * @author hp
+ */
+public class Astring {
+
+
+    public static void main(String[] args) {
+        String st=new String();
+        String st1=new String("Welcome");
+        char c[]={'w','e','l','c','o','m','e'};
+        String st2=new String(c,3,4);
+        System.out.println(st);
+        System.out.println(st1);
+        System.out.println(st2);
+        
+    }   
+}
+
+
